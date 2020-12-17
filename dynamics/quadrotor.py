78c864e0d11ab8fd4af731c7685f75bb4a5c2e55
sd196821 <sd196821@gmail.com>
2020-12-17 11:31:58 +0800

    first

---
+++ dynamics/quadrotor.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy
+
+class Drone():
+    """Quadrotor class"""
+
+    def __init__(self):
+        self.mass = 2.0
+        self.inertia = np.array([[10, 0, 0],
+                                 [0, 10, 0],
+                                 [0, 0, 10]])
+        self.Nstate = 13 #x,y,z,vx,vy,vz,q1,q2,q3,q4,w1,w2,w3
+        self.state =
