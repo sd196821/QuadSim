da0067c0c6ecf5caf1fc26410afbe3b943a71ce9
wanghl <sd196821@gmail.com>
2021-01-02 22:26:53 +0800

    Add hovering_env

--- gym-drone/gym_docking/__init__.py
+++ gym-drone/gym_docking/__init__.py
@@ -3,3 +3,7 @@ from gym.envs.registration import register
 register(id='docking-v0',
          entry_point='gym_docking.envs:DockingEnv',
          )
+
+register(id='hovering-v0',
+         entry_point='gym_docking.envs:HoveringEnv',
+         )
---
+++ gym-drone/gym_docking/envs/hovering_env.py
@@ -0,0 +1,25 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+
+class HoveringEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+
+
+    def step(self, action):
+
+
+    def reset(self):
+
+
+    def render(self):
+
+
+    def close(self):
+
+
+
+
