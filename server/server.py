e22a49f6947f9bc6233bf8ac351ce1b5cd15a7c6
wanghl <sd196821@gmail.com>
2021-03-23 20:05:06 +0800

    add unity server ver 0.9, almost done

--- server/server.py
+++ server/server.py
@@ -1,12 +1,24 @@
 import zmq
 import time
 import random
+import google.protobuf.text_format
+import DroneMsg_pb2 as msg
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind("tcp://*:12345")
 
+message = msg.MsgDrone()
+message.id = 1
+message.sim_step = 1
+
+t = 0
+
 while True:
-    message = str(random.uniform(-1.0, 1.0)) + " " + str(random.uniform(-1.0, 1.0)) + " " + str(random.uniform(-1.0, 1.0))
-    socket.send_string(message)
-    print(message)
+    t += 1
+    message.sim_step = t
+    # message = str(random.uniform(-1.0, 1.0)) + " " + str(random.uniform(-1.0, 1.0)) + " " + str(random.uniform(-1.0, 1.0))
+    data = message.SerializeToString()
+    socket.send(data)
+    # socket.send_string(data)
+    print(data)
     time.sleep(1)
