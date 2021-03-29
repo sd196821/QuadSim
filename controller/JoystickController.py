afc3e2b3236989ce17a4e11103129e49043ac042
wanghl <sd196821@gmail.com>
2021-03-29 17:16:58 +0800

    add joystick controller. almost done. Todo : 1. transfer video stream; 2.add target drone; 3.Joystick to pid controller

--- controller/JoystickController.py
+++ controller/JoystickController.py
@@ -2,11 +2,6 @@ from evdev import InputDevice, categorize, ecodes, KeyEvent
 
 gamepad = InputDevice('/dev/input/event28')
 
-last = {
-        "ABS_RZ": 128,
-        "ABS_Z": 128
-    }
-
 for event in gamepad.read_loop():
     if event.type == ecodes.EV_ABS:
         absevent = categorize(event)
