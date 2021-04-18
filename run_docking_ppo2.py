07deba440e5f6930fef44afcf806726342372104
wanghl <sd196821@gmail.com>
2021-04-18 10:32:39 +0800

    Almost Finished Docking Environment

--- run_docking_ppo2.py
+++ run_docking_ppo2.py
@@ -4,10 +4,10 @@ from stable_baselines import PPO2
 from stable_baselines.common.vec_env import DummyVecEnv, VecNormalize
 # from stable_baselines.common.evaluation import evaluate_policy
 
-env = DummyVecEnv([lambda: gym.make("gym_docking:docking-v0")])
-# gym.make('gym_docking:hovering-v0')
+#env = DummyVecEnv([lambda: gym.make("gym_docking:docking-v0")])
+env = gym.make('gym_docking:hovering-v0')
 
 model = PPO2('MlpPolicy', env, verbose=1, tensorboard_log="./ppo2_docking_tensorboard/",
              learning_rate=2.5e-2)
-model.learn(total_timesteps=100000)
+model.learn(total_timesteps=1000000)
 model.save("ppo2_docking")
