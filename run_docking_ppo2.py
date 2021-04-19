6060a8d2049bd1317e393ff36d2d5f3ecb5094dd
wanghl <sd196821@gmail.com>
2021-04-19 14:10:43 +0800

    tunning

--- run_docking_ppo2.py
+++ run_docking_ppo2.py
@@ -3,55 +3,12 @@ import gym
 from stable_baselines import PPO2
 from stable_baselines.common.vec_env import DummyVecEnv, VecNormalize, SubprocVecEnv
 from stable_baselines.common import set_global_seeds, make_vec_env
+from stable_baselines.common.callbacks import CheckpointCallback
 # from stable_baselines.common.evaluation import evaluate_policy
 import tensorflow as tf
 
 # env = DummyVecEnv([lambda: gym.make("gym_docking:docking-v0")])
 # env = gym.make('gym_docking:docking-v0')
-
-# class SaveOnBestTrainingRewardCallback(BaseCallback):
-#     """
-#     Callback for saving a model (the check is done every ``check_freq`` steps)
-#     based on the training reward (in practice, we recommend using ``EvalCallback``).
-#
-#     :param check_freq: (int)
-#     :param log_dir: (str) Path to the folder where the model will be saved.
-#       It must contains the file created by the ``Monitor`` wrapper.
-#     :param verbose: (int)
-#     """
-#     def __init__(self, check_freq: int, log_dir: str, verbose=1):
-#         super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
-#         self.check_freq = check_freq
-#         self.log_dir = log_dir
-#         self.save_path = os.path.join(log_dir, 'best_model')
-#         self.best_mean_reward = -np.inf
-#
-#     def _init_callback(self) -> None:
-#         # Create folder if needed
-#         if self.save_path is not None:
-#             os.makedirs(self.save_path, exist_ok=True)
-#
-#     def _on_step(self) -> bool:
-#         if self.n_calls % self.check_freq == 0:
-#
-#           # Retrieve training reward
-#           x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-#           if len(x) > 0:
-#               # Mean training reward over the last 100 episodes
-#               mean_reward = np.mean(y[-100:])
-#               if self.verbose > 0:
-#                 print("Num timesteps: {}".format(self.num_timesteps))
-#                 print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
-#
-#               # New best model, you could save the agent here
-#               if mean_reward > self.best_mean_reward:
-#                   self.best_mean_reward = mean_reward
-#                   # Example for saving best model
-#                   if self.verbose > 0:
-#                     print("Saving new best model to {}".format(self.save_path))
-#                   self.model.save(self.save_path)
-#
-#         return True
 
 
 def make_env(env_id, rank, seed=0):
@@ -82,6 +39,9 @@ if __name__ == '__main__':
 
     # model = ACKTR(MlpPolicy, env, verbose=1)
 
+    checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='./logs/',
+                                             name_prefix='rl_model')
+
     model = PPO2(policy='MlpPolicy', env=env, verbose=1,
                  tensorboard_log="./ppo2_docking_tensorboard/",
                  policy_kwargs=dict(
@@ -91,14 +51,17 @@ if __name__ == '__main__':
                  # n_steps=math.floor(cfg['env']['max_time'] / cfg['env']['ctl_dt']),
                  n_steps=1000,
                  ent_coef=0.00,
-                 learning_rate=3e-2,
+                 learning_rate=3e-3,
                  vf_coef=0.5,
                  max_grad_norm=0.5,
                  nminibatches=1,
                  noptepochs=10,
                  cliprange=0.2)
-    model.learn(total_timesteps=500000)
-    model.save("ppo2_docking")
+    # load trained model
+    model.load("./ppo2_docking.zip", env=env, tensorboard_log="./ppo2_docking_tensorboard/")
+
+    model.learn(total_timesteps=500000, callback=checkpoint_callback)
+    model.save("ppo2_docking_500K_1M")
 
 
 # model.learn(total_timesteps=250000)
--- run_trained_docking_ppo2.py
+++ run_trained_docking_ppo2.py
@@ -10,9 +10,18 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from utils.transform import quat2rot, rot2euler, euler2rot, rot2quat, rad2deg, deg2rad, quat2euler
 
+def info2array(info,tf):
+    chaser_st = np.zeros((tf, 13))
+    target_st = np.zeros((tf, 13))
+    for i in range(tf):
+        chaser_st[i, :] = info[i]['chaser']
+        target_st[i, :] = info[i]['target']
+
+    return chaser_st, target_st
+
 #env = DummyVecEnv([lambda: gym.make("gym_docking:docking-v0")])
 env =  gym.make('gym_docking:docking-v0')
-model = PPO2.load('ppo2_docking')
+model = PPO2.load('ppo2_docking_500K_1M')
 
 total_step = 1000
 state = np.zeros((total_step, 12))
@@ -21,6 +30,7 @@ time = np.zeros(total_step)
 u_all = np.zeros((total_step, 4))
 done = False
 tf = 0
+info_lst = []
 
 obs = env.reset()
 for t in range(total_step):
@@ -35,6 +45,7 @@ for t in range(total_step):
     # rpy[t, :] = quat2eul(state_now[6:9]))
 
     time[t] = t
+    info_lst.append(info)
     # print('obs:', obs)
     # print('vel:', state_now[3:6])
     # print('euler:', state_now[6:9])
@@ -42,10 +53,10 @@ for t in range(total_step):
     # print(state_now)
     # print(state)
     tf = t
-    #if done:
-        #obs = env.reset()
+    if done:
+        obs = env.reset()
     #    tf = t
-        #break
+        break
 
 
 print(state.shape)
@@ -105,4 +116,25 @@ ax.set_xlabel("relative x")
 ax.set_ylabel("relative y")
 ax.set_zlabel("relative z")
 
+#plt.figure()
+chaser_st, target_st = info2array(info_lst,tf)
+# chaser = info_states
+# print(chaser_st)
+#plt.plot(time[0:tf], (info_lst[0:tf])['chaser'])
+
+chaser_traj_fig = plt.figure()
+ax = Axes3D(chaser_traj_fig)
+ax.plot3D(chaser_st[0:tf, 0], chaser_st[0:tf, 1], chaser_st[0:tf, 2])
+ax.set_xlabel("chaser x")
+ax.set_ylabel("chaser y")
+ax.set_zlabel("chaser z")
+
+target_traj_fig = plt.figure()
+ax = Axes3D(target_traj_fig)
+ax.plot3D(target_st[0:tf, 0], target_st[0:tf, 1], target_st[0:tf, 2])
+ax.set_xlabel("target x")
+ax.set_ylabel("target y")
+ax.set_zlabel("target z")
+
 plt.show()
+
